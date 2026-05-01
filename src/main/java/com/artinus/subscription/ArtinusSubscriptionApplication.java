cd3017578aa4b08d711f15c651fec617fb252c72
yht0827 <yangheetae0827@naver.com>
2026-05-01 17:45:30 +0900

    초기 프로젝트 구성

---
+++ src/main/java/com/artinus/subscription/ArtinusSubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.artinus.subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtinusSubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtinusSubscriptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artinus/subscription/ArtinusSubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artinus.subscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtinusSubscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
